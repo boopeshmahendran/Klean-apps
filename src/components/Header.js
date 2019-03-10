997a92f279d2d5485a1e44cdd49f83ac6d92f2e6
Boopesh <boopesh@Boopeshs-MacBook-Pro.local>
2019-03-10 20:40:09 +0530

    Style header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,37 @@
 import React from "react";
-import logo from "../images/Logo.svg";
+import styles from "./Header.css";
+import logo from "../images/logo.svg";
+import home_icon from "../images/home_icon.svg";
+import about_us_icon from "../images/about_us_icon.svg";
+import services_icon from "../images/services_icon.svg";
+import contact_us_icon from "../images/contact_us_icon.svg";
 
 class Header extends React.Component {
     render() {
         return (
-            <header>
-                <div>
+            <header className={styles.header}>
+                <div className={styles.header__brand}>
                     <img src={logo} alt="logo" />
+                    <h2>klean apps</h2>
                 </div>
-                <nav>
+                <nav className={styles.header__navigation}>
                     <ul>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Services</li>
-                        <li>Contact Us</li>
+                        <li className={styles.header__navigation_item}>
+                            <img src={home_icon} alt="home icon"/>
+                            Home
+                        </li>
+                        <li className={styles.header__navigation_item}>
+                            <img src={about_us_icon} alt="about us icon"/>
+                            About Us
+                        </li>
+                        <li className={styles.header__navigation_item}>
+                            <img src={services_icon} alt="services icon"/>
+                            Services
+                        </li>
+                        <li className={styles.header__navigation_item}>
+                            <img src={contact_us_icon} alt="contact us icon"/>
+                            Contact Us
+                        </li>
                     </ul>
                 </nav>
             </header>
