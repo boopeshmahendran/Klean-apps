e8145b7d64d8d3aedc6d2bb882a29d6b8df0edca
Boopesh <boopesh@boopeshs-mbp.corp.adobe.com>
2019-03-23 12:50:45 +0530

    Create strings file for storing strings

---
+++ src/strings.js
@@ -0,0 +1,9 @@
+export default {
+    BRAND_NAME: "Klean apps",
+    ADDRESS_TEXT: "Address",
+    ADDRESS: "No. 123, abcd street, Bangalore - 1234",
+    PHONE_TEXT: "Phone",
+    PHONE: "1234567890",
+    EMAIL_TEXT: "Email",
+    EMAIL: "abcd@gmail.com"
+}
