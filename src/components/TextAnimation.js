4ad5c8ffb4a2e284d1335d5f9986e075080a49fc
Boopesh <boopesh@Boopeshs-MacBook-Pro.local>
2019-03-23 23:46:28 +0530

    Add logic for TextAnimation

--- src/components/TextAnimation.js
+++ src/components/TextAnimation.js
@@ -1,14 +1,74 @@
 import React from "react";
-import styles from "./TextAnimation.css"
+import styles from "./TextAnimation.css";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 class TextAnimation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeIndex: 0
+        };
+
+        this.animationInterval = null;
+    }
+
+    componentDidMount() {
+        this.startAnimation();
+    }
+    
+    componentWillUnmount() {
+        this.stopAnimation();
+    }
+
+    startAnimation() {
+        this.animationInterval = setInterval(() => {
+            this.setState({
+                activeIndex: (this.state.activeIndex + 1) % this.props.values.length
+            });
+        }, this.props.animationDelay);
+    }
+
+    stopAnimation() {
+        clearInterval(this.animationInterval);
+    }
+
     render() {
-        const values = this.props.values;
-        console.log(values);
+        const valuesElList = this.props.values.map((value, index) => {
+            const classes = cx({
+                textAnimationValue: true,
+                active: index === this.state.activeIndex
+            });
+
+            return <span key={index} className={classes}>{value}</span>
+        });
+
+        const translateAmt = ((-100 * this.state.activeIndex) / this.props.values.length);
+
+        const sliderStyles = {
+            transform: `translateY(${translateAmt}%)`
+        };
+
         return (
-            <span className={styles.textAnimation}>Klean</span>
+            <span className={styles.textAnimation}>
+                <span className={styles.slider} style={sliderStyles}>
+                    {valuesElList}
+                </span>
+            </span>
         )
     }
+}
+
+TextAnimation.defaultProps = {
+    animationDelay: 4000
+}
+
+TextAnimation.propTypes = {
+    values: PropTypes.arrayOf(PropTypes.string).isRequired,
+    animationDelay: PropTypes.number
 }
 
 export default TextAnimation;
