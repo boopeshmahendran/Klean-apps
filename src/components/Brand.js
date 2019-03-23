2ec6bfe8bca209821cdae78c78ab6f1a129ff523
Boopesh <boopesh@Boopeshs-MacBook-Pro.local>
2019-03-24 00:47:49 +0530

    Make font size as prop

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -2,16 +2,29 @@ import React from "react";
 import styles from "./Brand.css";
 import logo from "../images/icons/logo.svg";
 import Strings from "../../strings";
+import PropTypes from "prop-types";
 
 class Brand extends React.Component {
     render() {
+        const style = {
+            fontSize: this.props.fontSize
+        };
+
         return (
-            <div className={styles.brand}>
+            <div className={styles.brand} style={style}>
                 <img className={styles.logo} src={logo} alt="logo" />
                 <div>{Strings.BRAND_NAME}</div>
             </div>
         );
     }
+}
+
+Brand.defaultProps = {
+    fontSize: "2rem"
+}
+
+Brand.propTypes = {
+    fontSize: PropTypes.string
 }
 
 export default Brand;
