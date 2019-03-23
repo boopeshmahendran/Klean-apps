659e7e601b742bdf0a2ad473e53f7546ae33d575
Boopesh <boopesh@boopeshs-mbp.corp.adobe.com>
2019-03-23 11:40:27 +0530

    Add align prop to headingline

--- src/components/HeadingLine.js
+++ src/components/HeadingLine.js
@@ -1,13 +1,28 @@
 import React from "react";
 import styles from "./HeadingLine.css"
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 
 class HeadingLine extends React.Component {
     render() {
+        const classes = classNames({
+            [styles.headingLine]: true,
+            [styles.alignLeft]: this.props.align === "left"
+        });
+
         return (
-            <hr className={styles.headingLine} />
+            <hr className={classes} />
         )
     }
 }
+
+HeadingLine.defaultProps = {
+    align: "center"
+};
+
+HeadingLine.propTypes = {
+    align: PropTypes.oneOf(["left", "center"]),
+};
 
 export default HeadingLine;
