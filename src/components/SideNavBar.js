7b2a98dca0d8c94362843759f7dafbde2ead5b2c
boopeshmahendran <boopeshmahendran@gmail.com>
2019-04-08 21:57:21 +0530

    Style side nav bar

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -54,32 +54,34 @@ class SideNavBar extends React.Component {
         return (
             <section className={classes} onClick={this.close}>
                 <div className={styles.sideNavContainer} onClick={this.blockClicks}>
-                    <a className={styles.closeBtn} href="#0" onClick={this.close}>
-                        <img src={close} alt="close icon" />
-                    </a>
-                    <Brand />
+                    <div className={styles.header}>
+                        <Brand />
+                        <a className={styles.closeBtn} href="#0" onClick={this.close}>
+                            <img src={close} alt="close icon" />
+                        </a>
+                    </div>
                     <nav className={styles.navigation}>
                         <ul>
-                            <li className={styles.navigation_item}>
-                                <a href="#" onClick={this.handleNavigation}>
+                            <li>
+                                <a className={styles.navigation_item} href="#" onClick={this.handleNavigation}>
                                     <img src={home} alt="home icon" />
                                     {Strings.HOME_TEXT}
                                 </a>
                             </li>
-                            <li className={styles.navigation_item}>
-                                <a href="#about" onClick={this.handleNavigation}>
+                            <li>
+                                <a className={styles.navigation_item} href="#about" onClick={this.handleNavigation}>
                                     <img src={about_us} alt="about us icon" />
                                     {Strings.ABOUT_US_TEXT}
                                 </a>
                             </li>
-                            <li className={styles.navigation_item}>
-                                <a href="#services" onClick={this.handleNavigation}>
+                            <li>
+                                <a className={styles.navigation_item} href="#services" onClick={this.handleNavigation}>
                                     <img src={services} alt="services icon" />
                                     {Strings.SERVICES_TEXT}
                                 </a>
                             </li>
-                            <li className={styles.navigation_item}>
-                                <a href="#getintouch" onClick={this.handleNavigation}>
+                            <li>
+                                <a className={styles.navigation_item} href="#getintouch" onClick={this.handleNavigation}>
                                     <img src={contact_us} alt="contact us icon" />
                                     {Strings.CONTACT_US_TEXT}
                                 </a>
