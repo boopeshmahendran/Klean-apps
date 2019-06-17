4fdd918a0d70c5b87f95acb28996366b8aa1f97e
boopeshmahendran <boopeshmahendran@gmail.com>
2019-06-18 01:17:01 +0530

    Fix navbar click scrolling

--- src/css-constants.js
+++ src/css-constants.js
@@ -4,7 +4,8 @@ module.exports = {
     primaryColor: '#494E9F',
     // primaryColor: '#cd53a8',
     // primaryColor: '#6fd4e1',
-    accentColor: '#FDAD31'
+    accentColor: '#FDAD31',
+    fixedHeaderHeight: '70px'
     // accentColor: '#ffbda5'
     // accentColor: '#b9e7e6'
 };
